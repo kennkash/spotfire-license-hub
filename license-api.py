51f2158aed0eeda6cf674112607ef936fc556a8c
kennkash <kkashmiry23@gmail.com>
2026-02-09 22:32:19 -0600

    Update license-api.py

--- license-api.py
+++ license-api.py
@@ -1,16 +1,50 @@
+[
+  {
+    "USER_NAME": "vdasika4950",
+    "USER_EMAIL": "v.dasika@samsung.com",
+    "LAST_ACTIVITY": "2025-12-16 17:25:05",
+    "ANALYST_FUNCTIONS": 29,
+    "NON_ANALYST_FUNCTIONS": 0,
+    "ANALYST_PCT": 100,
+    "ANALYST_USER_FLAG": true,
+    "ANALYST_THRESHOLD": 50,
+    "ANALYST_ACTIONS_PER_DAY": 0.3222,
+    "ANALYST_ACTIONS_PER_ACTIVE_DAYS": 29,
+    "ACTIVE_DAYS": 1,
+    "cost_center_name": "T1 MIE",
+    "dept_name": "MIE RMG/MOL",
+    "title": "Senior Engineer",
+    "TITLE_CATEGORY": "Engineer"
+  },
+  {
+    "USER_NAME": "ymao",
+    "USER_EMAIL": "yingjun.mao@samsung.com",
+    "LAST_ACTIVITY": "2025-12-14 08:54:17",
+    "ANALYST_FUNCTIONS": 0,
+    "NON_ANALYST_FUNCTIONS": 0,
+    "ANALYST_PCT": 0,
+    "ANALYST_USER_FLAG": false,
+    "ANALYST_THRESHOLD": 50,
+    "ANALYST_ACTIONS_PER_DAY": 0,
+    "ANALYST_ACTIONS_PER_ACTIVE_DAYS": 0,
+    "ACTIVE_DAYS": 0,
+    "cost_center_name": "T1 MIE",
+    "dept_name": "MIE",
+    "title": "Staff Engineer I TR",
+    "TITLE_CATEGORY": "Engineer"
+  },
+
+
 from fastapi import APIRouter, Query, HTTPException
 from typing import List, Optional, Dict, Any
 import pandas as pd
 from pathlib import Path
 
-router = APIRouter(prefix="/v0", tags=["license-reduction"])
+router = APIRouter()
 
-CSV_PATH = Path(__file__).resolve().parents[1] / "data" / "license_reduction.csv"
 
 def load_df() -> pd.DataFrame:
-    if not CSV_PATH.exists():
-        raise HTTPException(status_code=500, detail=f"CSV not found at {CSV_PATH}")
-    df = pd.read_csv(CSV_PATH)
+    df = pd.read_csv('http://s3.api.com:9090/spotfire-admin/analyst-functions-users.csv')
 
     # normalize headers (trim + lowercase)
     df.columns = [c.strip() for c in df.columns]
@@ -35,11 +69,6 @@ def get_license_reduction(cost_center_name: str = Query(...)) -> List[Dict[str, 
 
     # filter
     filtered = df[df["cost_center_name"].astype(str).str.strip() == cost_center_name].copy()
-
-    # OPTIONAL: map/clean types here
-    # e.g. if column exists as string, convert to numeric
-    if "est_savings_usd" in filtered.columns:
-        filtered["est_savings_usd"] = pd.to_numeric(filtered["est_savings_usd"], errors="coerce").fillna(0).astype(int)
 
     # return list of dicts
     return filtered.fillna("").to_dict(orient="records")
